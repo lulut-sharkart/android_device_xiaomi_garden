4deb084b34d11f72a9fe1b1d77a32b2516c50b02
xyzuan <xyzuannihboss@gmail.com>
2021-10-28 13:20:46 +0000

    garden:add missing permissions required to boot

--- device.mk
+++ device.mk
@@ -117,3 +117,8 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_AAPT_CONFIG := xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xhdpi
+
+# [DNM] Temp permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/xyz.extras.xml \
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/xyz.extras.xml
